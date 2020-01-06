eae5b2ec3cd8227f8244936076ee25713429019e
Manoochehr Joodi Bigdello <43603619+mjbigdel@users.noreply.github.com>
2020-01-06 12:28:00 +0100

    Add files via upload
    the GUI is in Persian

---
+++ Vokala/Crack/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Location = new System.Drawing.Point(68, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "تولید کد فعال سازی";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(68, 175);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "کد فعال سازی";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(26, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "جهت دریافت کد فعال سازی کد سخت افزاری را وارد کنید";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "کد سخت افزاری";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 219);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Vokala/Crack/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ramz(string s)
+        {
+            char[] ch = new char[11];
+            for (int i = 0; i < 5; i++)
+            {
+                ch[i] = (char)(s[i] + 5);
+            }
+
+            ch[5] = '-';
+            for (int i = 6; i < 11; i++)
+            {
+                ch[i] = (char)(s[i] + 10);
+            }
+
+
+            string s2 = new string(ch);
+            return s2;
+        }
+
+        public string ramz2(string s)
+        {
+            char[] ch = new char[10];
+            for (int i = 0; i < ch.Length; i++)
+            {
+                ch[i] = (char)(s[i] + i);
+            }
+            string s2 = new string(ch);
+            return s2;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //table1TableAdapter.FillBy(dataSet11.Table1, textBox1.Text);
+            //if (dataSet11.Table1.Rows.Count == 0)
+            //    MessageBox.Show("کد محصول اشتباه است");
+            //else if (int.Parse((dataSet11.Table1.Rows[0]["installcount"]).ToString()) < 3)
+            //{
+
+                textBox3.Text = ramz2(textBox2.Text);
+                //table1TableAdapter.InsertQuery (textBox1.Text , dataSet11.Table1.Rows[0]["installcount"] ,textBox2.Text );
+
+            //}
+            //else
+            //{
+            //    textBox3.Text = ramz(textBox2.Text);
+
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+// TODO: This line of code loads data into the 'dataSet11.Table1' table. You can move, or remove it, as needed.
+        //this.table1TableAdapter.Fill(this.dataSet11.Table1);
+
+          //   this.table1TableAdapter.Fill(this.dataSet11.Table1);               
+            
+        }
+    }
+}
